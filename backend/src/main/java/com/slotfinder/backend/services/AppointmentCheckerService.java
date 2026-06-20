e518bc0ce7130e74939ff37d41c2005ac432d144
wasif laskar <wasiflaskar@iMac.local>
2026-06-20 14:33:51 -0700

    Add Comm100 API connectivity test

--- backend/src/main/java/com/slotfinder/backend/services/AppointmentCheckerService.java
+++ backend/src/main/java/com/slotfinder/backend/services/AppointmentCheckerService.java
@@ -1,6 +1,7 @@
 package com.slotfinder.backend.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -11,6 +12,16 @@ public class AppointmentCheckerService {
 
     public List<AppointmentSlot> checkForAppointments() {
 
+        RestTemplate restTemplate = new RestTemplate();
+
+String response =
+    restTemplate.getForObject(
+        "https://api13.comm100.io/booking/services/b5ae0403bdef420aa87f92b39fa73b7b/availableDatesandtimes?siteId=80000203&timezone=Pacific%20Standard%20Time",
+        String.class
+    );
+
+System.out.println(response);
+
         AppointmentSlot slot1 = new AppointmentSlot();
 
         slot1.setAdvisorName("Andrew");
@@ -20,5 +31,7 @@ public class AppointmentCheckerService {
 
         return List.of(slot1);
     }
+
+
 
 }
